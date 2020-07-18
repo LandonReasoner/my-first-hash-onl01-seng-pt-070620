5d0336cf22e16f2c2d5051958dd9d3806153e94c
Landon <reasoner.landon90@gmail.com>
2020-07-18 01:00:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,6 @@
 def my_hash
-  hash = {"name" => "landon" , "age" => "24"}
+ hash = {"name" => "landon" , "age" => "24"}
+ end
 end# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 
